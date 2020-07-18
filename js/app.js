729e97448a3174046ef95694e56253938122572c
Beans <pscbus_103@Beans-MBA.local>
2020-07-18 01:44:42 -0400

    created event listener and defined vars

---
+++ js/app.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const squares = document.querySelectorAll('.grid div')
+    const resultDisplay = document.querySelector('#result')
+    let width = 15
+    let currentShooterIndex = 200
+    let currentShooter2Index = 204
+    let currentInvaderIndex = 0
+    let alienInvadersTakenDown = []
+    let result = 0
+    let direction = 1
+    let invaderId
+
+        const alienInvaders = [
+                //define aliens\\
+            0,1,2,3,4,5,6,7,8,9,15,16,
+            17,18,19,20,21,22,23,24,30,
+            31,32,33,34,35,36,37,38,39
+        ]
+
+        
+
+        
+}) 
